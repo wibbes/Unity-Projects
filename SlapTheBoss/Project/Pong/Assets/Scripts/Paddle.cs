5d7e80bc74132e006e370a21a1be17686dd9807c
wibbes <jamwillyo@gmail.com>
2017-09-24 12:34:17 +0100

    24/09/17

---
+++ SlapTheBoss/Project/Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    [Header("Object[s")]
+    public Camera MainCamera;
+
+    [Header("Prefab's")]
+    public GameObject PaddlePrefab;
+
+    [Header("Paddle Location")]
+    public float PaddleOffsetX;
+    public float PaddleOffsetY;
+
+    [HideInInspector]
+    public GameObject UserPaddle;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector2 PaddleOffestLocation = MainCamera.ScreenToWorldPoint.(PaddleOffset);
+        PaddleOffestLocation.x = PaddleOffsetX;
+        Instantiate(PaddlePrefab, new Vector3(PaddleOffestLocation.x,PaddleOffestLocation.y,0), Quaternion.identity);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
